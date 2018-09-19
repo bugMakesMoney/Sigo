fbc70cf6b831c94f6fed9baf031eaa9af198d17e
kinddfull <kinddfull@gmail.com>
2018-09-19 18:36:07 +0900

    remove load .env values

--- src/SigoBot/SigoBot.ts
+++ src/SigoBot/SigoBot.ts
@@ -1,14 +1,13 @@
 import { ConfigModel, AppConfigModel } from './model/ConfigModel'
 import { appConfig } from './config/appConfig'
-import { loadConfig } from './util/config'
 import { clientConfig } from './config/clientConfig'
 
 class SigoBot {
   app: appConfig
   client: clientConfig
-  constructor(_config?: ConfigModel, _server?: any) {
-    const config: ConfigModel = _config ? _config : loadConfig()
-    this.app = new appConfig(config)
+
+  constructor(_config: ConfigModel, _server?: any) {
+    this.app = new appConfig(_config)
     this.client = new clientConfig(_server)
   }
 
