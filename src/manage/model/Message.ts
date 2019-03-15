18c217225198dc5f2c6e7174aca37d3bef0f56b4
kindd <kinddfull@gmail.com>
2019-03-15 12:18:12 +0900

    save user name

--- src/manage/model/Message.ts
+++ src/manage/model/Message.ts
@@ -3,15 +3,17 @@ import { Document, Schema, model } from 'mongoose'
 interface IMessage extends Document {
   index: number
   userId: string
+  userName: string
   date: Date
   text?: string
   imageUrl?: string
-  paylod?: string
+  payload?: string
 }
 
 const MessageSchema = new Schema({
   index: Number,
   userId: String,
+  userName: String,
   date: Date,
   text: String,
   imageUrl: String,
@@ -27,17 +29,20 @@ export default class Message {
 
   static saveMessage = async ({
     userId,
+    userName,
     text,
     imageUrl,
     payload,
   }: {
     userId: string
+    userName?: string
     text?: string
     imageUrl?: string
     payload?: string
   }) => {
     return await MessageModel.create({
       userId,
+      userName,
       text,
       date: new Date(),
       imageUrl,
